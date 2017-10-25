f9c02a2a9c06158ad60b75f37984a54baadc46ff
dancyfang <dancyfang@gmail.com>
2017-10-24 23:21:33 -0400

    Create 2.py

---
+++ 2.py
@@ -0,0 +1,27 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        root = n = ListNode(0) # copy root, shallow copy
+        carry = 0
+        while l1 or l2 or carry: 
+            v1 = v2 = 0 # handle None value
+            if l1:
+                v1 = l1.val
+                l1 = l1.next
+            if l2:
+                v2 = l2.val
+                l2 = l2.next
+            carry, val = divmod(v1+v2+carry,10)
+            n.next = ListNode(val) # if we use: n.val = ListNode(val) n.next = ListNode(0) n = n.next   we might get an extra 0 before result
+            n = n.next
+        return root.next 
