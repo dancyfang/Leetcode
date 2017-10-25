82addca88f7c2e27ec4c595595e165d031b084bf
dancyfang <dancyfang@gmail.com>
2017-10-24 22:34:07 -0400

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for i in nums:
+            if target-i in nums:
+                return [nums.index(i),nums.index(target-i)]
