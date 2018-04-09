24b9050669c7e5192707ed49be3742bf55fe17d4
dancyfang <dancyfang@gmail.com>
2018-04-09 15:54:09 -0400

    Create 230-kth smallest in BST.py

---
+++ binarySearch/230-kth smallest in BST.py
@@ -0,0 +1,30 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+# binary search, count number of nodes
+class Solution:
+    def kthSmallest(self, root, k):
+        """
+        :type root: TreeNode
+        :type k: int
+        :rtype: int
+        """
+        if root is None:
+            return
+        c = self.count(root.left)
+        if c > k-1:
+            return self.kthSmallest(root.left, k)
+        elif c == k-1:
+            return root.val
+        else:
+            return self.kthSmallest(root.right, k-c-1)
+        
+    def count(self, root):
+        if root is None:
+            return 0
+        else:
+            return self.count(root.left) + self.count(root.right) + 1
